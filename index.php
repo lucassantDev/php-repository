391bbb001594687b68666893df91f8183177356a
Lucas Sant <lucasfdesantana095@gmail.com>
2026-02-22 16:51:45 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo 'PHP'
+        ?>
+    </h1>
+    <p>
+        O título é em PHP
+    </p>
+    
+</body>
+</html>
